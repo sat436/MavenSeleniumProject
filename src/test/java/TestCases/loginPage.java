d48b3394346f8bfe73f7f508ec6600e67100982f
sat436 <8008519058.satish@gmail.com.com>
2018-10-09 18:44:03 -0400

    added new TEST CASES package for home,login & added new class to previous package

---
+++ src/test/java/TestCases/loginPage.java
@@ -0,0 +1,10 @@
+package TestCases;
+
+public class loginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Test case for login page");
+	}
+
+}
---
+++ src/test/java/mavenseleniumprojectpackage/DemoTest2.java
@@ -0,0 +1,11 @@
+package mavenseleniumprojectpackage;
+
+public class DemoTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New demo class Test2 is added");
+
+	}
+
+}
